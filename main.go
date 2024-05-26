2f228f643b59d2e47d46b71cc767c94ff7456aed
Sergei Volkov <serg-v@toloka.ai>
2024-05-26 08:00:41 +0200

    build command

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"encoding/json"
 	"flag"
@@ -7,11 +8,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
-
-type Project struct {
-	Name string `json:"name"`
-}
 
 type Projects struct {
 	Count    int       `json:"count"`
@@ -20,21 +18,45 @@ type Projects struct {
 
 func main() {
 	command := flag.String("command", "list", "Command to execute")
+	id := flag.String("id", "", "ID of the project or configuration to use")
+	branch := flag.String("branch", "", "Branch to use")
 	flag.Parse()
+
+	c := NewCredentials()
+
 	switch *command {
 	case "list":
-		listProjects()
+		listProjects(c)
+	case "list-builds":
+		listConfigurations(c, *id)
+	case "build":
+		buildConfiguration(c, *id, *branch)
 	default:
 		fmt.Println("Unknown command")
 	}
 }
 
-func listProjects() {
-	host := os.Getenv("TEAMCITY_HOST")
-	login := os.Getenv("TEAMCITY_LOGIN")
-	password := os.Getenv("TEAMCITY_PASSWORD")
+type Credentials struct {
+	Username string
+	Password string
+	Host     string
+}
 
-	url := fmt.Sprintf("https://%s/httpAuth/app/rest/projects", host)
+func NewCredentials() Credentials {
+	return Credentials{
+		Host:     os.Getenv("TEAMCITY_HOST"),
+		Username: os.Getenv("TEAMCITY_LOGIN"),
+		Password: os.Getenv("TEAMCITY_PASSWORD"),
+	}
+}
+
+type Project struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func listProjects(c Credentials) {
+	url := fmt.Sprintf("https://%s/httpAuth/app/rest/projects", c.Host)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -42,7 +64,7 @@ func listProjects() {
 		return
 	}
 
-	req.SetBasicAuth(login, password)
+	req.SetBasicAuth(c.Username, c.Password)
 	req.Header.Set("Accept", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
@@ -64,8 +86,90 @@ func listProjects() {
 		fmt.Println(err)
 		return
 	}
-	// print each project on a new line
+	// print each project with id on a new line
 	for _, project := range projects.Projects {
-		fmt.Println(project.Name)
+		fmt.Println("ID:", project.Id, " Name:", project.Name)
 	}
+}
+
+type BuildType struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	ProjectName string `json:"projectName"`
+	ProjectId   string `json:"projectId"`
+	Href        string `json:"href"`
+	WebUrl      string `json:"webUrl"`
+}
+
+type Configurations struct {
+	Count      int         `json:"count"`
+	BuildTypes []BuildType `json:"buildType"`
+}
+
+func listConfigurations(c Credentials, id string) {
+	url := fmt.Sprintf("https://%s/httpAuth/app/rest/projects/id:%s/buildTypes", c.Host, id)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.SetBasicAuth(c.Username, c.Password)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var configurations Configurations
+	err = json.Unmarshal(body, &configurations)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// print each configuration with id on a new line
+	for _, configuration := range configurations.BuildTypes {
+		fmt.Println("ID:", configuration.Id, " Name:", configuration.Name)
+	}
+}
+
+func buildConfiguration(c Credentials, id, branch string) {
+	url := fmt.Sprintf("https://%s/httpAuth/app/rest/buildQueue", c.Host)
+
+	// Form the XML body
+	var body string
+	if branch != "" {
+		body = fmt.Sprintf(`<build branchName="%s"><buildType id="%s"/></build>`, branch, id)
+	} else {
+		body = fmt.Sprintf(`<build><buildType id="%s"/></build>`, id)
+	}
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(body))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.SetBasicAuth(c.Username, c.Password)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/xml")
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Build started")
 }
